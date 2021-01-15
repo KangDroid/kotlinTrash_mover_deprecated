57bca3918d12de804be226122b22cbb996e25a81
KangDroid <hyunwoo286@gmail.com>
2021-01-16 00:25:41 +0900

    Initial Start Point
    TRASK-1 #done

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    val springBootVersion = "2.1.7.RELEASE"
+
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:${springBootVersion}")
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "me.kangdroid"
+version = "1.0-SNAPSHOT"
+
+apply {
+    plugin("org.springframework.boot")
+    plugin("io.spring.dependency-management")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/com/kangdroid/main/MainEntry.kt
@@ -0,0 +1,7 @@
+package com.kangdroid.main
+
+/**
+ * Main Entry
+ */
+fun main(args: Array<String>) {
+}
